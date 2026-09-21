fefa9c474c73274352e9d07e9bfe3933f45de734
viresh anand <vireshanand278@gmail.com>
2026-09-21 23:30:47 +0530

    Add linked list practice

---
+++ src/linkedlist.java
@@ -0,0 +1,21 @@
+public class linkedlist {
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public  Node head;
+    public Node tail;
+
+    public addFirst(int data){
+
+    }
+    public static void main(String[] args) {
+        linkedlist linkedlist = new linkedlist();
+
+    }
+}
